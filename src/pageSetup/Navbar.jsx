e722d71ca82373431bbf7c29bc315c2b384e4b4f
Aspasija <aspasija.cvetkoska@result.si>
2024-07-13 00:29:45 +0200

    changed logo to a pokeball

--- src/pageSetup/Navbar.jsx
+++ src/pageSetup/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import LogoImg from "../Images/charmander.png";
+import LogoImg from "../Images/pokeball.png";
 
 const NavbarComponent = () => {
   return (
