ed7853ceee78e54e7e01c67520510c6d19d3ad76
Aspasija <aspasija.cvetkoska@result.si>
2024-07-13 17:19:24 +0200

    added like favorite button

--- src/components/ListModal.jsx
+++ src/components/ListModal.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
+import { Heart } from "react-bootstrap-icons";
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
 import ProgressBar from "react-bootstrap/ProgressBar";
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 
@@ -48,7 +49,7 @@ const ListModal = ({ show, onHide, pokemon }) => {
 
   return (
     <Modal
-    size="lg"
+      size="lg"
       show={show}
       onHide={onHide}
       aria-labelledby="pokemon-modal-title"
@@ -77,126 +78,132 @@ const ListModal = ({ show, onHide, pokemon }) => {
       <Modal.Body className="text-start">
         <Container>
           <Row>
-            <Col sm={4}>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-          alt={pokemon.name}
-          style={{ width: "200px", marginBottom: "20px" }}
-          className="align-center"
-        />
-        </Col>
-        <Col sm={8}>
-        <Tabs
-          defaultActiveKey="about"
-          id="pokemon-modal-tabs"
-          className="mb-3"
-          fill
-        >
-          <Tab eventKey="about" title="About">
-            <div className="text-start mt-3">
-              <p>
-                <strong>Height:</strong> {pokemon.height * 10} cm
-              </p>
-              <p>
-                <strong>Weight:</strong> {pokemon.weight / 10} kg
-              </p>
-            </div>
-          </Tab>
-          <Tab eventKey="base-stats" title="Base Stats">
-            <div className="mt-3">
-              {pokemon.stats.map((stat) => (
-                <div key={stat.stat.name} className="mb-3">
-                  <div className="d-flex justify-content-between">
-                    <strong>{capitalize(stat.stat.name)}</strong>
-                    <span>{stat.base_stat}</span>
+            <Col
+              sm={4}
+              className="d-flex flex-column justify-content-start align-items-center mb-2"
+            >
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                alt={pokemon.name}
+                style={{ width: "200px", marginBottom: "20px" }}
+                className="align-center"
+              />
+              <Button variant="light">
+                <Heart />
+              </Button>
+            </Col>
+            <Col sm={8}>
+              <Tabs
+                defaultActiveKey="about"
+                id="pokemon-modal-tabs"
+                className="mb-3"
+                fill
+              >
+                <Tab eventKey="about" title="About">
+                  <div className="text-start mt-3">
+                    <p>
+                      <strong>Height:</strong> {pokemon.height * 10} cm
+                    </p>
+                    <p>
+                      <strong>Weight:</strong> {pokemon.weight / 10} kg
+                    </p>
                   </div>
-                  <ProgressBar
-                    now={stat.base_stat}
-                    label={`${stat.base_stat}`}
-                    className="mt-2"
-                    variant="warning"
-                  />
-                </div>
-              ))}
-            </div>
-          </Tab>
-          <Tab eventKey="attacks" title="Attacks">
-            <div className="mt-3">
-              {pokemon.moves.slice(0, 6).map((move, index) => (
-                <div key={index} className="mb-3">
-                  <div className="d-flex justify-content-between">
-                    <strong>{capitalize(move.move.name)}</strong>
-                    <span>{move.move.power || "-"}</span>
+                </Tab>
+                <Tab eventKey="base-stats" title="Base Stats">
+                  <div className="mt-3">
+                    {pokemon.stats.map((stat) => (
+                      <div key={stat.stat.name} className="mb-3">
+                        <div className="d-flex justify-content-between">
+                          <strong>{capitalize(stat.stat.name)}</strong>
+                          <span>{stat.base_stat}</span>
+                        </div>
+                        <ProgressBar
+                          now={stat.base_stat}
+                          label={`${stat.base_stat}`}
+                          className="mt-2"
+                          variant="warning"
+                        />
+                      </div>
+                    ))}
                   </div>
-                  <ProgressBar
-                    now={move.move.power || 50}
-                    label={`${move.move.power || "-"}`}
-                    className="mt-2"
-                    variant="danger"
-                  />
-                </div>
-              ))}
-            </div>
-          </Tab>
-          <Tab eventKey="evolutions" title="Evolutions">
-            <div className="mt-3">
-              {evolutions.map((evolution, index) => (
-                <div
-                  key={index}
-                  className="d-flex flex-column align-items-center mb-3"
-                >
-                  <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolution.id}.png`}
-                    alt={evolution.name}
-                    style={{ width: "100px", marginBottom: "10px" }}
-                  />
-                  <strong>{capitalize(evolution.name)}</strong>
-                </div>
-              ))}
-            </div>
-          </Tab>
-          <Tab eventKey="encounters" title="Encounters">
-            <div className="mt-3">
-              {encounters.length === 0 ? (
-                <Card body>
-                  <p>No encounters found for this Pokemon.</p>
-                </Card>
-              ) : (
-                encounters.map((encounter, index) => (
-                  <div key={index} className="mb-3">
-                    <Card body>
-                      <p>
-                        <strong>Location:</strong>{" "}
-                        {capitalize(encounter.location_area.name)}
-                      </p>
-                      <p>
-                        <strong>Method:</strong>{" "}
-                        {capitalize(
-                          encounter.version_details[0].encounter_details[0]
-                            .method.name
-                        )}
-                      </p>
-                      <p>
-                        <strong>Level Range:</strong>{" "}
-                        {
-                          encounter.version_details[0].encounter_details[0]
-                            .min_level
-                        }{" "}
-                        -{" "}
-                        {
-                          encounter.version_details[0].encounter_details[0]
-                            .max_level
-                        }
-                      </p>
-                    </Card>
+                </Tab>
+                <Tab eventKey="attacks" title="Attacks">
+                  <div className="mt-3">
+                    {pokemon.moves.slice(0, 6).map((move, index) => (
+                      <div key={index} className="mb-3">
+                        <div className="d-flex justify-content-between">
+                          <strong>{capitalize(move.move.name)}</strong>
+                          <span>{move.move.power || "-"}</span>
+                        </div>
+                        <ProgressBar
+                          now={move.move.power || 50}
+                          label={`${move.move.power || "-"}`}
+                          className="mt-2"
+                          variant="danger"
+                        />
+                      </div>
+                    ))}
                   </div>
-                ))
-              )}
-            </div>
-          </Tab>
-        </Tabs>
-        </Col>
-        </Row>
+                </Tab>
+                <Tab eventKey="evolutions" title="Evolutions">
+                  <div className="mt-3">
+                    {evolutions.map((evolution, index) => (
+                      <div
+                        key={index}
+                        className="d-flex flex-column align-items-center mb-3"
+                      >
+                        <img
+                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${evolution.id}.png`}
+                          alt={evolution.name}
+                          style={{ width: "100px", marginBottom: "10px" }}
+                        />
+                        <strong>{capitalize(evolution.name)}</strong>
+                      </div>
+                    ))}
+                  </div>
+                </Tab>
+                <Tab eventKey="encounters" title="Encounters">
+                  <div className="mt-3">
+                    {encounters.length === 0 ? (
+                      <Card body>
+                        <p>No encounters found for this Pokemon.</p>
+                      </Card>
+                    ) : (
+                      encounters.map((encounter, index) => (
+                        <div key={index} className="mb-3">
+                          <Card body>
+                            <p>
+                              <strong>Location:</strong>{" "}
+                              {capitalize(encounter.location_area.name)}
+                            </p>
+                            <p>
+                              <strong>Method:</strong>{" "}
+                              {capitalize(
+                                encounter.version_details[0]
+                                  .encounter_details[0].method.name
+                              )}
+                            </p>
+                            <p>
+                              <strong>Level Range:</strong>{" "}
+                              {
+                                encounter.version_details[0]
+                                  .encounter_details[0].min_level
+                              }{" "}
+                              -{" "}
+                              {
+                                encounter.version_details[0]
+                                  .encounter_details[0].max_level
+                              }
+                            </p>
+                          </Card>
+                        </div>
+                      ))
+                    )}
+                  </div>
+                </Tab>
+              </Tabs>
+            </Col>
+          </Row>
         </Container>
       </Modal.Body>
       <Modal.Footer>
